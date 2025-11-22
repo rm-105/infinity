e64b0197f39db78202aa59e57bb472f7398c9ea0
rm-100 <pyaesone20psp@gmail.com>
2025-11-22 20:04:57 +0630

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nan20psp/maythusharmusicv1",
+    "https://github.com/rm-100/infinity",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
